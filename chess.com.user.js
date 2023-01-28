5efd849496e438ebbfeecd8fad969ec873ce14c4
darwinikii <74075511+darwinikii@users.noreply.github.com>
2023-01-28 14:47:07 +0300

    Update chess.com.user.js

--- chess.com.user.js
+++ chess.com.user.js
@@ -5,8 +5,10 @@
 // @description  chess.com ai bot
 // @author       You
 // @match        https://www.chess.com/game/*
+// @match        https://www.chess.com/play/computer
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chess.com
-// @grant        none
+// @grant GM_setValue
+// @grant GM_getValue
 // ==/UserScript==
 
 (async function() {
@@ -20,18 +22,48 @@
     var board = document.getElementsByClassName("board")[0]
     var highlight1 = document.createElement("div")
     highlight1.className = "darwins highlight square-18"
-    highlight1.style = "background-color: rgb(0, 0, 255); opacity: 0.5;"
+    highlight1.style = "background-color: rgb(255, 0, 50); opacity: 0.5; box-shadow: 0 0 7px #FF0032, 0 0 10px #FF0032, 0 0 21px #FF0032, 0 0 42px #FF0032, 0 0 82px #FF0032, 0 0 92px #FF0032, 0 0 102px #FF0032, 0 0 151px #FF0032;"
     board.appendChild(highlight1)
     var highlight2 = document.createElement("div")
     highlight2.className = "darwins highlight square-88"
-    highlight2.style = "background-color: rgb(0, 0, 255); opacity: 0.5;"
+    highlight2.style = "background-color: rgb(0, 255, 170); opacity: 0.5; box-shadow: 0 0 7px #00FFAA, 0 0 10px #00FFAA, 0 0 21px #00FFAA, 0 0 42px #00FFAA, 0 0 82px #00FFAA, 0 0 92px #00FFAA, 0 0 102px #00FFAA, 0 0 151px #00FFAA;"
     board.appendChild(highlight2)
-    setInterval(getFen, 2000);
+    var highlight3 = document.createElement("div")
+    highlight3.className = "darwins highlight square-18"
+    highlight3.style = "background-color: #A020F0; opacity: 0.5; box-shadow: 0 0 7px #A020F0, 0 0 10px #A020F0, 0 0 21px #A020F0, 0 0 42px #A020F0, 0 0 82px #A020F0, 0 0 92px #A020F0, 0 0 102px #A020F0, 0 0 151px #A020F0;"
+    board.appendChild(highlight3)
+    var highlight4 = document.createElement("div")
+    highlight4.className = "darwins highlight square-88"
+    highlight4.style = "background-color: #FFA500; opacity: 0.5; box-shadow: 0 0 7px #FFA500, 0 0 10px #FFA500, 0 0 21px #FFA500, 0 0 42px #FFA500, 0 0 82px #FFA500, 0 0 92px #FFA500, 0 0 102px #FFA500, 0 0 151px #FFA500;"
+    board.appendChild(highlight4)
+    var btn = document.createElement("button")
+    btn.type = "button"
+    btn.className = GM_getValue("alternativeMove", false) ? "darwins-btn ui_v5-button-component ui_v5-button-primary" : "darwins-btn ui_v5-button-component ui_v5-button-basic"
+    btn.innerText = "Show alternative moves"
+    btn.style = "margin-left: 190px; height: 40px;"
+    document.getElementsByClassName("player-component")[1].appendChild(btn)
+    btn.addEventListener("click", function() {
+        var data = GM_getValue("alternativeMove", false);
+        if (data == false) {
+            GM_setValue("alternativeMove", true);
+            btn.className = "darwins-btn ui_v5-button-component ui_v5-button-primary"
+        } else {
+            GM_setValue("alternativeMove", false);
+            btn.className = "darwins-btn ui_v5-button-component ui_v5-button-basic"
+        }
+    });
+    setInterval(getFen, 3000);
 })();
 
 async function getFen() {
     var darwins = {}
     darwins.pieces = document.getElementsByClassName("piece")
+    if (!document.getElementsByClassName("selected")[0]) {
+        for (let hl of document.getElementsByClassName("darwins")) {
+            hl.style.opacity = 0
+        }
+        return
+    }
     darwins.table = {
         8: [null, null, null, null, null, null, null, null],
         7: [null, null, null, null, null, null, null, null],
@@ -41,14 +73,25 @@ async function getFen() {
         3: [null, null, null, null, null, null, null, null],
         2: [null, null, null, null, null, null, null, null],
         1: [null, null, null, null, null, null, null, null],
-        "activeColor": document.getElementsByClassName("vertical-move-list")[0].lastChild.children[2] == undefined ? "b" : "w",
+        "activeColor": document.getElementsByClassName("selected")[0].classList[0] == "white" ? "b" : "w",
         "castling": "-",
         "enPassant": "-",
         "halfmoveClock": 0,
         "fullmoveNumber": document.getElementsByClassName("vertical-move-list")[0].childElementCount
     }
-    if (document.getElementsByClassName("board")[0].className.includes("flipped") && darwins.table.activeColor == "w") return
-    if (!document.getElementsByClassName("board")[0].className.includes("flipped") && darwins.table.activeColor == "b") return
+    var isAlternative = GM_getValue("alternativeMove", false)
+    if (document.getElementsByClassName("board")[0].className.includes("flipped") && darwins.table.activeColor == "w") {
+        for (let hl of document.getElementsByClassName("darwins")) {
+            hl.style.opacity = 0
+        }
+        return
+    }
+    if (!document.getElementsByClassName("board")[0].className.includes("flipped") && darwins.table.activeColor == "b") {
+        for (let hl of document.getElementsByClassName("darwins")) {
+            hl.style.opacity = 0
+        }
+        return
+    }
     for (let piece of darwins.pieces) {
         if (piece.classList[2].length == 2) {
             piece.className = piece.classList[0] + " " + piece.classList[2] + " " + piece.classList[1]
@@ -112,15 +155,25 @@ async function getFen() {
         fen += jsonTable.halfmoveClock + " " + jsonTable.fullmoveNumber;
         return fen;
     }
-    var res = await makeRequest("GET", "http://127.0.0.1:8000/chess?fen=" + jsonToFen(darwins.table))
-    var [highlight1, highlight2] = document.getElementsByClassName("darwins")
+    var res = await makeRequest("GET", "http://127.0.0.1:8000/chess?fen=" + jsonToFen(darwins.table) + "&isAlternative=" + isAlternative.toString())
+    var [highlight1, highlight2, highlight3, highlight4] = document.getElementsByClassName("darwins")
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
     res = JSON.parse(res).msg
+    console.log(res)
     res[0].Move = res[0].Move.replace(res[0].Move.charAt(0), letters.indexOf(res[0].Move.charAt(0)) + 1)
     res[0].Move = res[0].Move.replace(res[0].Move.charAt(2), letters.indexOf(res[0].Move.charAt(2)) + 1)
-    res[1].Move = res[1].Move.replace(res[1].Move.charAt(0), letters.indexOf(res[1].Move.charAt(0)) + 1)
-    res[1].Move = res[1].Move.replace(res[1].Move.charAt(2), letters.indexOf(res[1].Move.charAt(2)) + 1)
     highlight1.className = "darwins highlight square-" + res[0].Move.substr(0,2)
     highlight2.className = "darwins highlight square-" + res[0].Move.substr(2,3)
     highlight2.innerText = res[0].Centipawn / 100
+    highlight1.style.opacity = 0.5
+    highlight2.style.opacity = 0.5
+    if (isAlternative) {
+        res[1].Move = res[1].Move.replace(res[1].Move.charAt(0), letters.indexOf(res[1].Move.charAt(0)) + 1)
+        res[1].Move = res[1].Move.replace(res[1].Move.charAt(2), letters.indexOf(res[1].Move.charAt(2)) + 1)
+        highlight4.className = "darwins highlight square-" + res[1].Move.substr(0,2)
+        highlight3.className = "darwins highlight square-" + res[1].Move.substr(2,3)
+        highlight3.innerText = (res[0].Centipawn / 100).toString() + " - Alternative"
+        highlight4.style.opacity = 0.5
+        highlight3.style.opacity = 0.5
+    }
 }
